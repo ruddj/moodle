a7c291a7ff5e330573087d0073bd62fd0626ba10
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2011-03-10 17:03:14 +1100

    SBHS SSO
    Signed-off-by: James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>

---
+++ auth/sbhs/lang/en/auth_sbhs.php
@@ -0,0 +1,12 @@
+<?php // $Id:
+      // auth_sbhs.php
+
+$string['pluginname'] = 'SBHS SSO';
+$string['auth_sbhstitle'] = 'SBHS SSO ';
+$string['auth_sbhsdescription'] = 'This method uses credentials given at student portal page to pass the username and password on to Moodle.';
+
+$string['auth_sbhsssofunc_key'] = 'Please type path to SSO functions';
+$string['auth_sbhsssofunc_eg'] = 'The php file where SSO functions are stored, eg /srv/www/sites/apps/sso/functions.php';
+
+$string['auth_sbhsredirect_key'] = 'Logout Redirect';
+$string['auth_sbhsredirect_desc'] = 'URL to redirect page to on logout';
