89af5b985daf04b663bbdacd234611792559f114
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2011-03-15 13:03:42 +1100

    Adding SBHS Library block for Moodle 2.0

---
+++ blocks/library/lang/en_utf8/block_library.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['go'] = 'Lookup';
+$string['newwindow'] = 'Open search results in a new window';
+$string['teacheronly'] = 'Hide this block from non-teachers';
+$string['pluginname'] = 'Library Enquiry';
+?>
