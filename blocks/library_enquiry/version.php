7e7891cd7146e1ac9934e227c4847dc00b2b9114
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2011-03-15 13:03:42 +1100

    Adding SBHS Library block for Moodle 2.0
    Signed-off-by: James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
    
    More 2.0 Compat
    
    Signed-off-by: James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
    
    Lang Add for 2.0
    
    Signed-off-by: James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
    
    Renamed Block
    
    Signed-off-by: James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
    
    Did not remove old library block
    
    Signed-off-by: James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>

---
+++ blocks/library_enquiry/version.php
@@ -0,0 +1,19 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+/**
+
+* SBHS Library Search
+* Written by James Rudd, 08/05/2007
+*
+* Based on Wikipedia Search  v2006100700 Author: David Horat ( david.horat@gmail.com )
+* based on work of Aggelos Panagiotakis( agelospanagiotakis@gmail.com )
+* Also Based on simpledocssearch v0.1 Written by Darren Smith (with some help from Marky Clarky) on 21/3/2006
+* Uses some code from Juilan Ridden, MonteNet
+* License: GPL
+*
+*
+*/
+
+$plugin->version = 2011031500;
