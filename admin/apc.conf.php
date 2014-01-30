a1dc2dd1535cc17b0a5e6c03e0651099f5d6a37b
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2014-01-30 17:00:23 +1100

    APC updated context ID

--- admin/apc.conf.php
+++ admin/apc.conf.php
@@ -5,7 +5,7 @@ require_once("../config.php");
 require_once($CFG->libdir.'/adminlib.php');
 
 require_login();
-require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM, SITEID));
+require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
 
 // Disable APC Auth
 defaults('USE_AUTHENTICATION',0);
