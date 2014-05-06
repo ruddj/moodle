bdf814183ad21e912ece088e92deb2dddf9ff5de
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2014-05-06 13:49:30 +1000

    SHS My Homework- Shorter create name

--- local/shsmyhomework/version.php
+++ local/shsmyhomework/version.php
@@ -1,11 +1,16 @@
 <?php
 
-$plugin->version  = 2013101206;   
-                                  
-                                  
-                                  
-                                  
-                                  
-$plugin->requires = 2013050100;  
-$plugin->cron     = 0;
+/**
+ *
+ * @package   SBHS Internal Website Homework Links
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2014020100;                             
+$plugin->requires = 2013050100;    // See http://docs.moodle.org/dev/Moodle_Versions for more information
+$plugin->cron     = 0;
+$plugin->component = 'local_shsmyhomework'; 
+$plugin->release   = '2.5';        // Human-friendly version name.
+$plugin->maturity  = MATURITY_RC;  // This version's maturity level.
