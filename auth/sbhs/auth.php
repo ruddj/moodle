5a48276919d4c2e4417cdf935043164c61c864c7
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2015-02-06 16:17:51 +1100

    Updated SBHS Auth

--- auth/sbhs/auth.php
+++ auth/sbhs/auth.php
@@ -37,10 +37,10 @@ class auth_plugin_sbhs extends auth_plugin_base {
         return false;
     }
 
-	/**
-	* Hook for overriding behavior of login page.
-	* This method is called from login/index.php page for all enabled auth plugins.
-	*/
+    /**
+    * Hook for overriding behavior of login page.
+    * This method is called from login/index.php page for all enabled auth plugins.
+    */
     function loginpage_hook() {
         global $frm;  // can be used to override submitted login form
         //global $user; // can be used to replace authenticate_user_login()
@@ -49,36 +49,54 @@ class auth_plugin_sbhs extends auth_plugin_base {
         if (empty($this->config->ssofunc)) {
             return;
         }
-	require_once($this->config->ssofunc);
-	if (sso_isLoggedIn()) {
-    if ($frm == NULL) {
-	    $frm = new stdClass;
-    }
-		$frm->username = sso_getUser();
-		$frm->password = sso_getPass();
-		return;
-	}
+        
+        require_once($this->config->ssofunc);
+        
+        if (sso_isLoggedIn()) {
+            if ($frm == NULL) {
+                $frm = new stdClass;
+            }
+                        
+            $frm->username = sso_getUser();
+            $frm->password = sso_getPass();
+            
+            // LDAP module SSO magic: 
+            // The logic to detect an SSO session is run whenever LDAP SSO is enabled, but the logic to attempt
+            // NTLM/Kerberos is only run on a request from an address matching a mask. So we can emulate this for
+            // when the password is not available -- this requires putting the session key in the password field and
+            // setting a magic flag.
+            // Other SHS SSO magic is to turn on LDAP SSO in Moodle, but set it to only apply for an impossibly small 
+            // subnet (recommended: 169.254.255.255/32). Also don't configure NTLM/Kerberos in Apache.
+            if ($frm->password == '') {   
+              //die($frm->username);           
+              $frm->password = sesskey();
+              $frm->username = strtolower($frm->username);
+              set_cache_flag('auth/ldap/ntlmsess', sesskey(), strtolower(sso_getUser()), 4); // 4 seconds
+            }
+            
+            return;
+        }
     }
 
-	/**
-	 * Hook for overriding behavior of logout page.
-	* This method is called from login/logout.php page for all enabled auth plugins.
-	* This will redirect you to the login page if you are not logged in to the Portal,
-	* but, if you are, you will be returned to the Portal instead
-	*/
+    /**
+     * Hook for overriding behavior of logout page.
+    * This method is called from login/logout.php page for all enabled auth plugins.
+    * This will redirect you to the login page if you are not logged in to the Portal,
+    * but, if you are, you will be returned to the Portal instead
+    */
     function logoutpage_hook() {
         global $USER;     // use $USER->auth to find the plugin used for login
-	global $redirect; // can be used to override redirect after logout
+        global $redirect; // can be used to override redirect after logout
 
-	// Return if SBHS enabled and settings not specified yet
-	if (empty($this->config->ssofunc)) {
-		return;
-	}
+        // Return if SBHS enabled and settings not specified yet
+        if (empty($this->config->ssofunc)) {
+           return;
+        }
 
-	require_once($this->config->ssofunc);
-	if (sso_isLoggedIn()) {
-		$redirect = $this->config->redirect;
-	}
+        require_once($this->config->ssofunc);
+        if (sso_isLoggedIn()) {
+            $redirect = $this->config->redirect;
+        }
     }
 
     /**
@@ -108,16 +126,16 @@ class auth_plugin_sbhs extends auth_plugin_base {
      */
     function process_config($config) {
         // set to defaults if undefined
-	if (!isset ($config->ssofunc)) {
+        if (!isset ($config->ssofunc)) {
             $config->ssofunc = '/srv/www/sites/apps/sso/functions.php';
         }
-	if (!isset ($config->redirect)) {
+        if (!isset ($config->redirect)) {
             $config->redirect = 'http://www.sydneyboyshigh.com/intranet/portal';
         }
 
         // save settings
-	set_config('ssofunc', $config->ssofunc, 'auth/sbhs');
-	set_config('redirect', $config->redirect, 'auth/sbhs');
+        set_config('ssofunc', $config->ssofunc, 'auth/sbhs');
+        set_config('redirect', $config->redirect, 'auth/sbhs');
 
         return true;
     }
