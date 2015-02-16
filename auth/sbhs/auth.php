c0f5e700713e43ee6ca24330577c27fcb88ff025
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2015-02-17 09:30:25 +1100

    SBHS SSO: Cleaned up code

--- auth/sbhs/auth.php
+++ auth/sbhs/auth.php
@@ -68,9 +68,13 @@ class auth_plugin_sbhs extends auth_plugin_base {
             // Other SHS SSO magic is to turn on LDAP SSO in Moodle, but set it to only apply for an impossibly small
             // subnet (recommended: 169.254.255.255/32). Also don't configure NTLM/Kerberos in Apache.
             if ($frm->password == '') {
-              $frm->password = sesskey();
-              $frm->username = strtolower($frm->username);
-              set_cache_flag('auth/ldap/ntlmsess', sesskey(), strtolower(sso_getUser()), 4); // 4 seconds
+              $username = core_text::strtolower(sso_getUser()); // Compatibility hack
+              $timeout  = 10; //AUTH_NTLMTIMEOUT
+              $sesskey  = sesskey();
+
+              $frm->password = $sesskey;
+              $frm->username = $username;
+              set_cache_flag('auth/ldap/ntlmsess', $sesskey, $username, $timeout); 
             }
 
             return;
