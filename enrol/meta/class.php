6ab4dc6115f82877e41460cdb04ad1526093cb65
James R <James.Rudd@sydneyboys-h.schools.nsw.edu.au>
2016-02-12 15:17:37 +1100

    Class MEta Gen Depreciated warning

--- enrol/meta/class.php
+++ enrol/meta/class.php
@@ -8,7 +8,7 @@ require('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 
 require_login();
-require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
+require_capability('moodle/site:config',  context_system::instance());
 
 // First get a list of meta courses matching "M-"
 $searchparam = 'M-%';
